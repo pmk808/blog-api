0773d9180e963feba1b846f89cb0ce1c67db8036
Peemak <cabanesmcphy123@gmail.com>
2025-01-23 13:59:39 +0800

    fix(db): fixed db connection

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ toolchain go1.23.1
 
 require (
 	github.com/gin-gonic/gin v1.10.0
+	github.com/google/uuid v1.6.0
 	gorm.io/driver/postgres v1.5.11
 	gorm.io/gorm v1.25.12
 )
--- internal/model/post.go
+++ internal/model/post.go
@@ -1,11 +1,24 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"gorm.io/gorm"
+)
 
 type Post struct {
-	ID        string    `gorm:"primaryKey" json:"id"`
-	Title     string    `json:"title"`
-	Slug      string    `json:"slug" gorm:"uniqueIndex"`
-	Content   string    `json:"content"`
-	CreatedAt time.Time `json:"createdAt"`
+	ID        uuid.UUID      `gorm:"type:uuid;primaryKey;default:gen_random_uuid()" json:"id"`
+	Title     string         `json:"title" binding:"required"`
+	Slug      string         `json:"slug" gorm:"uniqueIndex" binding:"required"`
+	Content   string         `json:"content" binding:"required"`
+	CreatedAt time.Time      `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt time.Time      `json:"updatedAt" gorm:"autoUpdateTime"`
+	DeletedAt gorm.DeletedAt `gorm:"index"`
+}
+
+// Automatically generate UUID before creating
+func (post *Post) BeforeCreate(tx *gorm.DB) (err error) {
+	post.ID = uuid.New()
+	return
 }
