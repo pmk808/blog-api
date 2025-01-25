c2eeab42e39835bdeb3c010154715464b0c843a2
Peemak <cabanesmcphy123@gmail.com>
2025-01-25 16:49:58 +0800

    huhuhu fix na pls

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -24,7 +24,7 @@ func main() {
 	r.Use(cors.New(cors.Config{
 		AllowOrigins: []string{
 			"http://localhost:5174",
-			"https://portfolio-mc-dev.vercel.app/",
+			"https://portfolio-mc-dev.vercel.app",
 		},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Accept", "Authorization", "X-API-Key"},
